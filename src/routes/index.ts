7e0b41f10e591544a7e3c9f7576e52c197b4ab77
Isaque de Sousa <isaque.error404@gmail.com>
2025-12-29 09:55:41 -0300

    feat(routes): integrate order routes into main router for handling order-related requests

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes';
+import orderRoutes from './order.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/orders', orderRoutes);
 
 export default router;
